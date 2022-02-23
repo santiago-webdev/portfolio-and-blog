0f33b32f0d9aa6997e766ce4c3bff57d64fbd90e
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-02-23 16:57:08 -0300

    Updates
    TODO fix scrolling animation still hides

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Intro from './components/intro/Intro'
+
+function App() {
+    return (
+        <>
+            <Intro />
+        </>
+    )
+}
+
+export default App
---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,47 @@
+import './intro.css'
+import Me from '../../img/mauricio-macri.jpg'
+
+const Intro = () => {
+    return (
+        <>
+            <div className='i'>
+                <div className='i-left'>
+                    <div className='i-left-wrapper'>
+                        <h2 className='i-intro'>
+                            <span className='hello'>Hello!</span> My name is
+                        </h2>
+                        <h1 className='i-name'>Santiago Gonzalez</h1>
+                        <div className='i-title'>
+                            <div className='i-title-wrapper'>
+                                <div className='i-title-item'>
+                                    Frontend Developer
+                                </div>
+                                <div className='i-title-item'>
+                                    Backend Developer
+                                </div>
+                            </div>
+                        </div>
+                        <p className='i-desc'>
+                            I'm interested in <strong>FOSS</strong>,
+                            <strong> Unix</strong> and computers, looking for my
+                            first job in the IT world. You can view my resume
+                            and few of my projects below
+                        </p>
+                    </div>
+                </div>
+                <div className='i-right'>
+                    <div className='i-card bg'></div>
+                    <div className='i-card'>
+                        <img
+                            src={Me}
+                            alt='change this Macri'
+                            className='i-img'
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Intro
