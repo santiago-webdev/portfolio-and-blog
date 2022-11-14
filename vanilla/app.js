6ec497a8fb29b931d46687b2de84a25bb4e494e0
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-11-14 20:49:02 -0300

    Updates

---
+++ vanilla/app.js
@@ -0,0 +1,12 @@
+// const card = document.querySelector('.link')
+// card.addEventListener('mouseover', changeTitleBG)
+// card.addEventListener('click', changeTitleBG)
+
+// function changeTitleBG() {
+//   card.style.backgroundColor = 'green'
+// }
+
+const obj = {
+  name: 'This website', // property name may be an identifier
+  status: 'this is the status', // or a number
+};
