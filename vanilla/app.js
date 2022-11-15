fe50e4d6a1b107f0cfd0cff1ad8feb6c940424c5
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-11-14 22:47:05 -0300

    Match the parent

--- vanilla/app.js
+++ vanilla/app.js
@@ -1,5 +1,5 @@
 const card = {
-  id: 4,
+  id: 'card4',
   title: 'This website',
   title_link: 'https://santigo-zero.github.io',
   title_description: "Santiago's Portfolio, this same website",
@@ -10,13 +10,13 @@ const card = {
   image: 'project_img_zap',
 }
 
-let objectList = [
-  card
-]
+let objectList = [card]
 
 function main() {
   const newCard = document.createElement('div')
   newCard.classList.add('card')
+  newCard.setAttribute('id', objectList[0].id)
+  newCard.insertAdjacentText('beforeend', 'test')
 
   // This is where we put all the cards
   const projectList = document.querySelector('.list-projects')
