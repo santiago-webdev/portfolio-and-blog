d128fd2ffe36efb8a535cb75486f644766375015
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-11-14 21:05:22 -0300

    WIP: Trying to populate list-projects with javascript

--- vanilla/app.js
+++ vanilla/app.js
@@ -10,3 +10,7 @@ const obj = {
   name: 'This website', // property name may be an identifier
   status: 'this is the status', // or a number
 };
+
+function load_home() {
+  document.getElementsByClassName("list-projects").innerHTML='<object type="text/html" data="../templates/card.html" ></object>';
+}
