a58d38c41b1b9a31d35c271f5360af81a2531e12
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-11-28 15:37:24 -0300

    Added Home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Home />
+
+<script>
+  import Home from './Home.svelte'
+</script>
