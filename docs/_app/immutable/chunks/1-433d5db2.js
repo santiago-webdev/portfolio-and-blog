2d04ed43abbf7578f043469a4f949e173966fd00
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-11-28 15:13:00 -0300

    yarn run build to docs/ instead of build/

---
+++ docs/_app/immutable/chunks/1-433d5db2.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-7124c06c.js";export{t as component};
---
+++ docs/_app/immutable/chunks/2-091380b5.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-fa1c36fc.js";export{t as component};
