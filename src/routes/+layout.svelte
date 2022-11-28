e400bf2daab8f137bfa79002754e4471c2e3f51c
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-11-28 15:07:21 -0300

    Updates
    Use prerendering for all pages
    Added reset.css and some global settings

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import '../reset.css';
+</script>
+
+<div class="wrapper">
+	<slot />
+</div>
+
+<style global>
+	:root {
+		--cc-fg: #e8e8e8;
+		--cc-bg: #16171d;
+		--cc-vanilla: #f3e5ab;
+		--cc-vanilla-red: #f38fa9;
+
+		--cc2-bg: #1b1a27;
+		--cc2-bg2: #232132;
+		--cc2-fg: #f9f8ff;
+
+		--cc2-header: #2b293c;
+
+		--cc2-bg-status: #363448;
+		--cc2-fg-status: #7c7b8b;
+
+		--cc3-fg: #c3c3c8;
+		--cc4-fg: #9da1b4;
+	}
+
+	.wrapper {
+		background-color: var(--cc-bg); /* FIXME This is not a fix I really like */
+	}
+
+	::selection {
+		color: #8ff3d9;
+		background-color: var(--cc-vanilla-red);
+	}
+
+	html {
+		font-family: 'Basier Circle', sans-serif;
+	}
+</style>
