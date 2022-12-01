a2c1c45a4bd938b693290d89d77469d1fd100e65
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-11-30 21:13:05 -0300

    Trying to reuse the cards from old commit 9f2e5c40f912d1505ba9795d1cf4b77a5cf4957a

--- src/routes/Projects.svelte
+++ src/routes/Projects.svelte
@@ -9,6 +9,81 @@
 			<h1>{title}</h1>
 			<h2>{subtitle}</h2>
 		</div>
+
+		<div class="list-projects">
+			<div class="card">
+				<div class="header">
+					<h2 class="card_title">
+						<a
+							target="_blank"
+							rel="noopener noreferrer"
+							title="Under construction"
+							class="link"
+							href="https://github.com/santigo-zero/santigo-zero.github.io"
+						>
+							Always a work in progress <i class="fa-solid fa-hammer" />
+						</a>
+					</h2>
+					<div class="status">
+						<div class="card_status">
+							<i class="fa-solid fa-check" />
+						</div>
+						<div class="card_status">STATUS</div>
+						<div class="card_status">STATUS</div>
+					</div>
+				</div>
+				<div class="project_img_first" />
+				<p class="card__description">There's more projects to come, just wait.</p>
+			</div>
+			<div class="card">
+				<div class="header">
+					<h2 class="card_title">
+						<a
+							target="_blank"
+							rel="noopener noreferrer"
+							title="Under construction"
+							class="link"
+							href="https://github.com/santigo-zero/santigo-zero.github.io"
+						>
+							Always a work in progress <i class="fa-solid fa-hammer" />
+						</a>
+					</h2>
+					<div class="status">
+						<div class="card_status">
+							<i class="fa-solid fa-check" />
+						</div>
+						<div class="card_status">STATUS</div>
+						<div class="card_status">STATUS</div>
+					</div>
+				</div>
+				<div class="project_img_first" />
+				<p class="card__description">There's more projects to come, just wait.</p>
+			</div>
+			<div class="card">
+				<div class="header">
+					<h2 class="card_title">
+						<a
+							target="_blank"
+							rel="noopener noreferrer"
+							title="Under construction"
+							class="link"
+							href="https://github.com/santigo-zero/santigo-zero.github.io"
+						>
+							Always a work in progress <i class="fa-solid fa-hammer" />
+						</a>
+					</h2>
+					<div class="status">
+						<div class="card_status">
+							<i class="fa-solid fa-check" />
+						</div>
+						<div class="card_status">STATUS</div>
+						<div class="card_status">STATUS</div>
+					</div>
+				</div>
+				<div class="project_img_first" />
+				<p class="card__description">There's more projects to come, just wait.</p>
+			</div>
+		</div>
 	</main>
 </div>
 
@@ -21,12 +96,14 @@
 	}
 
 	main {
-		border-color: red;
-		border-style: solid;
+		/* border-color: red; */
+		/* border-style: solid; */
 
-		max-width: var(--content-width);
+		/* max-width: 66rem; */
 		padding: var(--padding);
 		padding-inline: var(--padding-inline);
+		max-width: 66rem;
+    width: 100%;
 	}
 
 	h1 {
@@ -42,5 +119,119 @@
 		font-size: clamp(1rem, 10vw, 1.5rem);
 		font-family: 'Inter', sans-serif;
 		font-weight: 400;
+	}
+
+	.list-projects {
+		display: grid;
+		/* margin-inline: 0.5em; */
+		gap: 1.05rem;
+		grid-auto-rows: minmax(33rem, auto);
+		grid-template-columns: repeat(auto-fill, minmax(min(20rem, 100%), 1fr));
+		/* max-width: 66rem; */
+    width: 100%;
+	}
+
+	.card {
+		background-color: var(--bg-light);
+		border-radius: 12px;
+		border-color: var(--bg-header);
+		border-style: solid;
+		border-width: 1px;
+
+		cursor: pointer;
+
+		overflow: auto;
+
+		gap: 0.6rem;
+
+		display: flex;
+		align-content: center;
+		flex-direction: column;
+		box-shadow: 3px 6px 9px rgba(0, 0, 0, 0.1);
+
+		animation: growCard 0.6s;
+		transition: all 0.1s ease-out;
+
+		border-style: solid;
+		border-color: orange;
+		border-width: 1px;
+	}
+
+	.card:hover {
+		transform: scale(1.03);
+	}
+
+	.card p {
+		margin-inline: 8px;
+		font-size: 1.2rem;
+		font-weight: 400;
+	}
+
+	@keyframes growCard {
+		from {
+			transform: scale(0.6);
+		}
+		to {
+			transform: scale(1);
+		}
+	}
+
+	.card_title {
+		color: var(--fg);
+		font-size: 1.4rem;
+		font-weight: 400;
+		text-align: center;
+		margin-bottom: 6px;
+	}
+
+	.card_status {
+		font-family: 'Inter', sans-serif;
+		box-shadow: 3px 6px 9px rgba(0, 0, 0, 0.1);
+		background-color: var(--bg-status);
+		color: var(--fg-status);
+		/* border-radius: 10px; */
+
+		border-top-left-radius: 6px 6px;
+		border-top-right-radius: 6px 6px;
+		border-bottom-right-radius: 6px 6px;
+		border-bottom-left-radius: 6px 6px;
+
+		padding: 3px 10px;
+		margin-inline: 2px;
+		/* padding-bottom: 5px; */
+	}
+
+	.header {
+		background-color: var(--bg-header);
+		padding: 6px;
+		padding-bottom: 10px;
+		/* background-color: green; */
+	}
+
+	.project_img_first {
+		background: rgb(34, 193, 195);
+		background: linear-gradient(114deg, rgba(34, 193, 195, 1) 0%, rgba(253, 187, 45, 1) 100%);
+
+		max-width: 100%;
+		height: 88px;
+		border-radius: 6px;
+		margin-inline: 8px;
+	}
+
+	.project_img_zap {
+		background: rgb(255, 228, 56);
+		background: linear-gradient(90deg, rgba(255, 228, 56, 1) 0%, rgba(252, 70, 87, 1) 100%);
+
+		max-width: 100%;
+		height: 88px;
+		border-radius: 6px;
+		margin-inline: 8px;
+	}
+
+	.status {
+		display: flex;
+		/* justify-content: center; */
+		flex-wrap: wrap;
+		gap: 0.4rem;
 	}
 </style>
