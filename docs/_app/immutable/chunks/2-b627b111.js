d2e6440174a4f698423114d2e0aa24da4778a5b5
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-12-03 07:30:55 -0300

    Build and format

---
+++ docs/_app/immutable/chunks/2-b627b111.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-47215874.js";export{t as component};
--- docs/_app/immutable/chunks/2-ed03c046.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-c7ae9547.js";export{t as component};
