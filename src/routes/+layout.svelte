1d622bb39ada796ec3d21db8e0d78f5708f15568
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-12-04 17:30:50 -0300

    Added static font Basier

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,47 +11,7 @@
 <Footer />
 
 <style global>
-	:global(*) {
-		--cc-fg: #e8e8e8;
-		--cc-bg: #16171d;
-
-		--cc-vanilla: #f3e5ab;
-		--cc-vanilla-red: #f38fa9;
-		--cc-dark-purple: #8e78aa;
-		--cc-light-purple: #c1abf3;
-		--cc-pale-blue: #abb9f3;
-
-		--cc2-bg: #1b1a27;
-		--cc2-bg2: #232132;
-		--cc2-fg: #f9f8ff;
-
-		--cc2-header: #2b293c;
-
-		--cc2-bg-status: #363448;
-		--cc2-fg-status: #7c7b8b;
-
-		--cc3-fg: #c3c3c8;
-		--cc4-fg: #9da1b4;
-
-		--max-content-width: 60rem;
-		--padding: 1.3rem;
-		--padding-inline: 0.6rem;
-	}
-
 	.wrapper {
 		color: var(--cc-fg);
-	}
-
-	h1 {
-		font-family: 'Basier Circle', sans-serif;
-	}
-
-	::selection {
-		color: #8ff3d9;
-		background-color: var(--cc-vanilla-red);
-	}
-
-	html {
-		font-family: 'Basier Circle', sans-serif;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,12 +11,14 @@
 <svelte:head>
 	<title>Santiago Gonzalez - Portfolio</title>
 
+  <link href="./global.css" rel="stylesheet" type="text/css" />
+
 	<!-- Fonts -->
-	<style>
-		@import url('https://fonts.cdnfonts.com/css/basier');
-		@import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700;900&display=swap');
-		@import url('https://fonts.cdnfonts.com/css/satoshi');
-	</style>
+	<!-- <style> -->
+	<!-- 	@import url('https://fonts.cdnfonts.com/css/basier'); -->
+	<!-- 	@import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700;900&display=swap'); -->
+	<!-- 	@import url('https://fonts.cdnfonts.com/css/satoshi'); -->
+	<!-- </style> -->
 	<!-- EOL Fonts -->
 
 	<!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
