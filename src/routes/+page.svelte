6e8501193e352c66331ada4e1f72bc0f932ba421
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-12-08 21:23:48 -0300

    moved all components under lib

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,17 @@
 <script>
 	import Nav from "$lib/Nav.svelte";
 
-	import Home from "./Home.svelte";
-	import Projects from "./Projects.svelte";
-	import About from "./About.svelte";
-	import Contact from "./Contact.svelte";
+	import Home from "$lib/components/Home.svelte";
+	// import Projects from "$lib/components/Projects.svelte";
+	// import About from "$lib/components/About.svelte";
+	// import Contact from "$lib/components/Contact.svelte";
 </script>
 
 <Nav />
-<Home />
-<Projects />
+<!-- <Home /> -->
+<!-- <Projects /> -->
 <!-- <About /> -->
-<Contact />
+<!-- <Contact /> -->
 
 <svelte:head>
 	<title>Santiago Gonzalez - Portfolio</title>
