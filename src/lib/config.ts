30c6fc0ed50b97584eb85a8a7e93659b5bd500c4
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-01-24 18:08:47 -0300

    Updates to font sizes

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,7 +4,7 @@ export const TITLE_BLOG_COMPOSE = ' - Santiago Gonzalez Blog';
 export const DESCRIPTION =
 	'Personal blog and portfolio, I talk about web development and show you my projects while at it.';
 export const DESCRIPTION_ALT =
-	'Here I talk about web development and show you my projects while at it.';
+	'Here we\'ll talk about web development and show you my projects while at it.';
 
 export const URL = 'https://santigo-zero.github.io/';
 export const REPO = 'https://github.com/santigo-zero/santigo-zero.github.io';
