615f8a32ddb215527f9d8c9b6f8f393dd0052bf0
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-01-24 17:23:58 -0300

    Added meta tags to +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,40 @@
 <script lang="ts">
 	import Presentation from '$lib/components/Presentation.svelte';
+	import { DESCRIPTION, TITLE, URL } from '$lib/config';
 </script>
 
 <main>
 	<Presentation />
 </main>
+
+<svelte:head>
+  <title>{TITLE}</title>
+
+  <!-- TODO(santigo-zero): meta tags for dynamic routing -->
+  <!-- HTML Meta Tags -->
+  <meta name="description" content={DESCRIPTION} />
+
+  <!-- Google / Search Engine Tags -->
+  <meta itemprop="name" content={TITLE} />
+  <meta itemprop="description" content={DESCRIPTION} />
+  <meta itemprop="image" content="" />
+  <!-- TODO(santigo-zero): Add image -->
+
+  <!-- Facebook Meta Tags -->
+  <meta property="og:url" content={URL} />
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content={TITLE} />
+  <meta property="og:description" content={DESCRIPTION} />
+  <meta property="og:image" content="" />
+  <!-- TODO(santigo-zero): Add image -->
+
+  <!-- Twitter Meta Tags -->
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:title" content={TITLE} />
+  <meta name="twitter:description" content={DESCRIPTION} />
+  <meta name="twitter:image" content="" />
+  <!-- TODO(santigo-zero): Add image -->
+</svelte:head>
 
 <style>
 	main {
