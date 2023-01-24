e0a717b2877169229cd0bbd11b15eb3413f44fba
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-01-24 16:00:19 -0300

    Added `Presentation`

---
+++ src/lib/components/Presentation.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	// <script context="module" lang="ts">
+</script>
+
+<h1>This is a title</h1>
+
+<style>
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,16 @@
-<h1>test</h1>
+<script lang="ts">
+	import Presentation from '$lib/components/Presentation.svelte';
+</script>
+
+<main>
+	<Presentation />
+</main>
+
+<style>
+	main {
+		/* min-height: min(100vh - 90px, 80rem); */
+		padding: var(--general-padding);
+		display: grid;
+		place-content: center;
+	}
+</style>
