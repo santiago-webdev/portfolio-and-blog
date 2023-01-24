afa236549ef2f022296415281773e0cbaf9ef691
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-01-24 15:31:37 -0300

    Keep font from previous commits and config some static files and global styling

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '$lib/styles/global.css'
+</script>
+
+<slot></slot>
+
+<svelte:head>
+	<link href="./typography.css" rel="stylesheet" type="text/css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+</svelte:head>
