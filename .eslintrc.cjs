8bcf959ee608a058f74446730915e8d95b935604
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-01-24 15:44:19 -0300

    Make general layout a grid and use 80 columns in formatter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,9 +8,7 @@ module.exports = {
 	],
 	plugins: ['svelte3', '@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [
-		{ files: ['*.svelte'], processor: 'svelte3/svelte3' }
-	],
+	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
 		'svelte3/typescript': () => require('typescript')
 	},
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,21 @@
 <script lang="ts">
+	import '$lib/styles/reset.css';
 	import '$lib/styles/global.css';
 </script>
 
-<slot />
+<div class="layout">
+	<slot />
+</div>
 
 <svelte:head>
-	<link
-		href="./typography.css"
-		rel="stylesheet"
-		type="text/css"
-	/>
-	<meta
-		name="viewport"
-		content="width=device-width, initial-scale=1"
-	/>
+	<link href="./typography.css" rel="stylesheet" type="text/css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
 </svelte:head>
+
+<style>
+	.layout {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+	}
+</style>
