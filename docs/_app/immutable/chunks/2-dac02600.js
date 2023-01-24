72e9ee101a9e67856012df0b6cfce6d0130a6cb3
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-01-24 14:28:22 -0300

    Restart this project

--- docs/_app/immutable/chunks/2-dac02600.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-d68be8d1.js";export{t as component};
--- src/routes/+layout.ts
+++
@@ -1,7 +0,0 @@
-// Prerender all pages
-export const prerender = true;
-
-/* Disable ssr because this page is meant to be completely static
- * https://kit.svelte.dev/docs/page-options#ssr
- */
-export const ssr = false;
