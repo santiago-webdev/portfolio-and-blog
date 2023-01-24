dd22da66a99974056ce79530201ad78e3d806c33
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-01-24 18:55:32 -0300

    More styling

--- src/lib/components/Presentation.svelte
+++ src/lib/components/Presentation.svelte
@@ -1,11 +1,13 @@
-<script lang="ts">
-	import { AUTHOR, DESCRIPTION_ALT } from '$lib/config';
-</script>
+<!-- <script lang="ts"> -->
+<!-- 	import { AUTHOR, DESCRIPTION_ALT } from '$lib/config'; -->
+<!-- </script> -->
 
 <section>
-	<h1>{AUTHOR}</h1>
+	<h1>Build websites && clean interfaces</h1>
 	<br />
-	<h2>{DESCRIPTION_ALT}</h2>
+	<h3>
+		Here we'll talk about web technologies and show you my projects while at it
+	</h3>
 </section>
 
 <style>
@@ -13,8 +15,14 @@
 		display: grid;
 		place-content: center;
 		min-height: min(100vh - 90px, 80rem);
-		text-align: center;
-		width: min(100% - 3rem, var(--wide));
+		/* text-align: center; */
+		width: min(70%, var(--wide));
 		margin-inline: auto;
+	}
+
+	h3 {
+		font-size: 1.4rem;
+		font-weight: 400;
+		width: 30ch;
 	}
 </style>
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,8 +3,6 @@ export const TITLE_BLOG = "Santiago's Blog";
 export const TITLE_BLOG_COMPOSE = ' - Santiago Gonzalez Blog';
 export const DESCRIPTION =
 	'Personal blog and portfolio, I talk about web development and show you my projects while at it.';
-export const DESCRIPTION_ALT =
-	'Here we\'ll talk about web development and show you my projects while at it.';
 
 export const URL = 'https://santigo-zero.github.io/';
 export const REPO = 'https://github.com/santigo-zero/santigo-zero.github.io';
