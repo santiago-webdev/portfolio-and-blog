471a703bd4f9cb6b77eae5b9c5323ebaee5349dd
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-01-24 15:48:38 -0300

    Added `Nav` component from my blog repository

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,83 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+	import { page } from '$app/stores';
+
+	let y = 0;
+	export let pageHeight = 0;
+	$: windowHeight = 0;
+	$: pageHeightWithoutWindow = pageHeight - windowHeight;
+
+	const navItems = [
+		{ label: 'Home', href: `${base}/` }
+		// { label: 'About', href: `${base}/about` },
+		// { label: 'Contact', href: `${base}/contact` }
+	];
+</script>
+
+<svelte:window bind:innerHeight={windowHeight} bind:scrollY={y} />
+
+<header style:position={$page.url.pathname === `/${base}` ? 'fixed' : 'sticky'}>
+	<a href="{base}/">Logo</a>
+	<nav>
+		<ul class="flex">
+			{#each navItems as item}
+				<li>
+					<a href={item.href}>{item.label}</a>
+				</li>
+			{/each}
+		</ul>
+	</nav>
+</header>
+<progress max={pageHeightWithoutWindow} value={y} />
+
+<style>
+	ul {
+		display: flex;
+		flex-direction: row;
+		place-items: center;
+	}
+
+	header {
+		top: 6px;
+		left: 0;
+		right: 0;
+		display: flex;
+		place-items: center;
+		place-content: center;
+		justify-content: space-between;
+		width: min(100% - 3rem, var(--wide));
+		margin-inline: auto;
+		/* outline: 3px red solid; */
+	}
+
+	a {
+		margin: 1rem;
+		font-weight: bold;
+		font-size: clamp(0.6rem, 8vw, 1.2rem);
+	}
+
+	progress {
+		top: 0;
+		left: 0;
+		right: 0;
+		position: fixed;
+		/* width: min(100% - 3rem, var(--wide)); */
+		width: 100%;
+		margin-inline: auto;
+		border-radius: 0;
+		height: 6px;
+		border: 0;
+	}
+
+	progress::-webkit-progress-bar,
+	progress {
+		background: transparent;
+	}
+
+	progress::-moz-progress-bar {
+		background: var(--cc-fg);
+	}
+	progress::-webkit-progress-value {
+		background: var(--cc-fg);
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,33 @@
 <script lang="ts">
 	import '$lib/styles/reset.css';
 	import '$lib/styles/global.css';
+	import Nav from '$lib/components/Nav.svelte';
+	import { onMount } from 'svelte';
+
+	let body: HTMLElement;
+	$: pageHeight = 0;
+	onMount(() => {
+		const resizeObserver = new ResizeObserver(entries => {
+			const entry = entries.at(0);
+
+			// Get the block size, which is basically like doing body.scrollHeight
+			// but with the difference that this will update if the height of the
+			// current page changes.
+			// pageHeight = Math.floor(entry!.contentBoxSize[0].blockSize)
+			if (entry !== undefined) {
+				pageHeight = Math.floor(entry.contentBoxSize[0].blockSize);
+			}
+		});
+
+		resizeObserver.observe(body);
+
+		// This callback cleans up the observer
+		return () => resizeObserver.unobserve(body);
+	});
 </script>
 
 <div class="layout">
+	<Nav {pageHeight} />
 	<slot />
 </div>
 
