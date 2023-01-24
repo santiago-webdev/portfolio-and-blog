25d317dab72a2423f352696bb6279561f506cffe
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-01-24 15:36:44 -0300

    Format

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { sveltekit } from "@sveltejs/kit/vite"
-import { defineConfig } from "vite"
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()],
-  test: {
-    include: ["src/**/*.{test,spec}.{js,ts}"]
-  },
-  server: {
-    fs: {
-      allow: [".."] // Allow serving files from one level up to the project root
-    }
-  }
-})
+	plugins: [sveltekit()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	server: {
+		fs: {
+			allow: ['..'] // Allow serving files from one level up to the project root
+		}
+	}
+});
