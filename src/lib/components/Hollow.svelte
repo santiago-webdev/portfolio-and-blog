2f078c0a77668f8d01adf7bd68e07db43cfe302e
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-01-25 22:43:03 -0300

    Fix general ::selection and text stroke for `Hollow`

--- src/lib/components/Hollow.svelte
+++ src/lib/components/Hollow.svelte
@@ -10,4 +10,10 @@
 		-webkit-text-stroke-width: 0.1rem;
 		-webkit-text-stroke-color: var(--fg2);
 	}
+
+	@media (max-width: 600px) {
+		span {
+			-webkit-text-stroke-width: 0.07rem;
+		}
+	}
 </style>
