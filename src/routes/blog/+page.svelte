50f7703cc6e2efa64f353a4d94ebb3aa371c73e9
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-01-27 21:53:41 -0300

    Update TITLE and use TITLE_BLOG for blog route

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
 	import Search from '$lib/components/Search.svelte';
+	import { TITLE_BLOG } from '$lib/config';
 </script>
 
 <main>
 	<Search />
 </main>
+
+<svelte:head>
+	<title>{TITLE_BLOG}</title>
+</svelte:head>
