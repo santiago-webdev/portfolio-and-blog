13c024c6d61b73a64c4835a36c60cdf525d48349
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-01-27 21:18:33 -0300

    Changes to `Template` for posts

--- src/lib/components/Template.svelte
+++ src/lib/components/Template.svelte
@@ -1,23 +1,58 @@
 <script lang="ts">
 	import { TITLE_BLOG_COMPOSE } from '$lib/config';
+	import { page } from '$app/stores'
 
 	export let title = '';
 	export let date = '';
+	export let description = '';
 </script>
 
 <article>
-	<h1>{title}</h1>
-	<p>Published: {date}</p>
+	{#if title}
+		<h1>{title}</h1>
+	{/if}
+
+	{#if date}
+		<p>PUBLISHED: {date}</p>
+	{/if}
 	<slot />
 </article>
 
+
 <svelte:head>
 	<title>{title}{TITLE_BLOG_COMPOSE}</title>
+
+	<!-- TODO(santigo-zero): meta tags for dynamic routing -->
+	<!-- HTML Meta Tags -->
+	<meta name="description" content={description} />
+
+	<!-- Google / Search Engine Tags -->
+	<meta itemprop="name" content={title} />
+	<meta itemprop="description" content={description} />
+	<meta itemprop="image" content="" />
+	<!-- TODO(santigo-zero): Add image -->
+
+	<!-- Facebook Meta Tags -->
+	<meta property="og:url" content={$page.url.toString()} />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+	<meta property="og:image" content="" />
+	<!-- TODO(santigo-zero): Add image -->
+
+	<!-- Twitter Meta Tags -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content={title} />
+	<meta name="twitter:description" content={description} />
+	<meta name="twitter:image" content="" />
+	<!-- TODO(santigo-zero): Add image -->
 </svelte:head>
 
 <style>
 	article {
 		display: grid;
 		place-content: center;
+		width: min(100% - 1rem, var(--wide));
+		margin-inline: auto;
 	}
 </style>
--- src/lib/posts/how-to-build-a-blog-in-sveltekit.md
+++ src/lib/posts/how-to-build-a-blog-in-sveltekit.md
@@ -1,3 +1,11 @@
 ---
 title: How this blog was built
+date: "2023-01-27 20:59"
+description: I'm going to explain to you how you can build your own static blog written in sveltekit
 ---
+
+# Here I'll explain how I built this blog using Sveltekit
+
+```bash
+yarn create svelte .
+```
