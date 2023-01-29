a3c2d480eeeef5b1229ee21fe79a8c4144c68930
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-01-29 14:32:48 -0300

    Disable modal box and progress bar

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
 	import { base } from '$app/paths';
-	import NavItems from './NavItems.svelte';
+	import { page } from '$app/stores';
 
 	export let pageHeight = 0;
 
 	let burger =
 		'<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 48 48"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M7.95 11.95h32m-32 12h32m-32 12h32"/></svg>';
-	let cross =
-		'<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 16 16"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m11.25 4.75l-6.5 6.5m0-6.5l6.5 6.5"/></svg>';
-	let modal: HTMLDialogElement;
-	const closeModal = () => {
-		if (modal) modal.open ? modal.close() : null;
-	};
-	const openModal = () => modal.showModal();
+	// let cross =
+	// 	'<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 16 16"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m11.25 4.75l-6.5 6.5m0-6.5l6.5 6.5"/></svg>';
+	// let modal: HTMLDialogElement;
+	// const closeModal = () => {
+	// 	if (modal) modal.open ? modal.close() : null;
+	// };
+	// const openModal = () => modal.showModal();
+	let toggleNav = false
 
 	let y = 0;
 	let windowHeight = 0;
@@ -25,34 +26,50 @@
 		{ label: 'About', href: `${base}/about` },
 		{ label: 'Contact', href: `${base}/contact` }
 	];
+
+	function matchBaseRoute(navItemLink: string, currentLink: string) {
+		if (navItemLink === '/') {
+			if (navItemLink === currentLink) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		return currentLink.match(navItemLink) !== null;
+	}
 </script>
 
 <svelte:window
 	bind:outerWidth={windowWidth}
 	bind:innerHeight={windowHeight}
 	bind:scrollY={y}
-	on:scroll={closeModal}
 />
 
 <header>
 	<a href="{base}/">Logo</a>
 	<nav>
-		{#if windowWidth > 700}
-			<NavItems {navItems} />
-		{:else}
-			<button on:click={openModal}>{@html burger}</button>
-			<dialog bind:this={modal}>
-				<NavItems {navItems} />
-				<button on:click={closeModal}>{@html cross}</button>
-			</dialog>
-		{/if}
+		<button on:click={() => toggleNav = !toggleNav}>{@html burger}</button>
+		<ul style:display={toggleNav ? '' : 'none'}>
+			{#each navItems as item}
+				<li>
+					<a
+						href={item.href}
+						class:activeRoute={matchBaseRoute(
+							item.href,
+							$page.url.pathname.toString()
+						)}>{item.label}</a
+					>
+				</li>
+			{/each}
+		</ul>
 	</nav>
-	<progress max={pageHeight - windowHeight} value={y} />
+	<!-- <progress max={pageHeight - windowHeight} value={y} /> -->
 </header>
 
 <style>
 	header {
-		top: 3px;
+		top: 0;
 		left: 0;
 		right: 0;
 		position: sticky;
@@ -61,9 +78,8 @@
 		justify-content: space-between;
 		width: min(100% - 1rem, var(--wide));
 		margin-inline: auto;
-		/* outline: 3px red solid; */
+		height: auto;
 		font-family: var(--ff-circle);
-		/* transition: all 0.3s cubic-bezier(0.07, 0.95, 0, 1); */
 	}
 
 	a {
@@ -84,37 +100,49 @@
 		color: var(--stone-900);
 	}
 
-	dialog {
-		border: 0.18rem solid var(--neutral-600);
-		border-radius: 0.6rem;
-		box-shadow: 0 0 1em rgb(0 0 0 / 0.3);
-		width: 100%;
-		height: 100%;
+	/* dialog { */
+	/* 	border: 0.18rem solid var(--neutral-600); */
+	/* 	border-radius: 0.6rem; */
+	/* 	box-shadow: 0 0 1em rgb(0 0 0 / 0.3); */
+	/* 	width: 100%; */
+	/* 	height: 100%; */
+	/* } */
+
+	/* progress { */
+	/* 	top: 0; */
+	/* 	left: 0; */
+	/* 	right: 0; */
+	/* 	position: fixed; */
+	/* 	width: 100%; */
+	/* 	margin-inline: auto; */
+	/* 	border-radius: 0; */
+	/* 	height: 10px; */
+	/* 	border: 0; */
+	/* } */
+
+	/* progress::-webkit-progress-bar, */
+	/* progress { */
+	/* 	background: transparent; */
+	/* } */
+
+	/* progress::-moz-progress-bar { */
+	/* 	background: var(--neutral-900); */
+	/* } */
+
+	/* progress::-webkit-progress-value { */
+	/* 	background: var(--neutral-900); */
+	/* } */
+
+	ul {
+		gap: 1rem;
+		display: flex;
+		flex-direction: row;
+		place-items: center;
 	}
 
-	progress {
-		top: 0;
-		left: 0;
-		right: 0;
-		position: fixed;
-		/* width: min(100% - 3rem, var(--wide)); */
-		width: 100%;
-		margin-inline: auto;
-		border-radius: 0;
-		height: 3px;
-		border: 0;
-	}
-
-	progress::-webkit-progress-bar,
-	progress {
-		background: transparent;
-	}
-
-	progress::-moz-progress-bar {
-		background: var(--neutral-900);
-	}
-
-	progress::-webkit-progress-value {
-		background: var(--neutral-900);
+	.activeRoute {
+		text-decoration: underline;
+		text-decoration-thickness: 0.2ch;
+		text-decoration-color: rgb(157, 161, 180);
 	}
 </style>
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -5,6 +5,7 @@
 
 <main>
 	<Search />
+	<Search />
 </main>
 
 <svelte:head>
