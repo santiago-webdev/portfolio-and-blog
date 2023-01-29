c62e84609cf14e61defa6ebb81278793cf2a3c86
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-01-29 18:45:35 -0300

    Logic for buttons and toggle for nav items done

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
 	import { base } from '$app/paths';
+	import { page } from '$app/stores';
 
 	let burger =
 		'<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 48 48"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M7.95 11.95h32m-32 12h32m-32 12h32"/></svg>';
 	let cross =
 		'<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 16 16"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m11.25 4.75l-6.5 6.5m0-6.5l6.5 6.5"/></svg>';
-	let mobile = false;
+	let showMobile = false;
 	let desktop = false;
 
-	const toggleMobile = () => mobile = !mobile
+	const toggleMobile = () => (showMobile = !showMobile);
 
 	let y = 0;
 	$: windowHeight = 0;
@@ -48,43 +49,37 @@
 	bind:scrollY={y}
 />
 
-<!-- <div class:blur-bg={y} class="header-wrapper"> -->
-<!-- <div -->
-<!-- 	style:background-color={y ? 'var(--neutral-500)' : 'transparent'} -->
-<!-- 	class="header-wrapper" -->
-<!-- > -->
-<div
-	style:background-color={true ? 'var(--neutral-500)' : 'transparent'}
-	class="header-wrapper"
->
-	<header>
-		<a href="{base}/">Logo</a>
-		{#if !desktop}
-			<button on:click={toggleMobile}>
-				{#if !mobile}
-					{@html burger}
-				{:else}
-					{@html cross}
-				{/if}
-			</button>
-		{/if}
-		<!-- {#if mobile} -->
-		<!-- 	<nav> -->
-		<!-- 		<ul> -->
-		<!-- 			{#each navItems as item} -->
-		<!-- 				<li> -->
-		<!-- 					<a -->
-		<!-- 						href={item.href} -->
-		<!-- 						class:activeRoute={matchBaseRoute( -->
-		<!-- 							item.href, -->
-		<!-- 							$page.url.pathname.toString() -->
-		<!-- 						)}>{item.label}</a -->
-		<!-- 					> -->
-		<!-- 				</li> -->
-		<!-- 			{/each} -->
-		<!-- 		</ul> -->
-		<!-- 	</nav> -->
-		<!-- {/if} -->
+<div class:blur-bg={y} class="header-wrapper">
+	<header style:flex-direction={desktop ? 'row' : 'column'}>
+		<div class="main-header">
+			<a href="{base}/">Logo</a>
+			{#if !desktop}
+				<button on:click={toggleMobile}>
+					{#if !showMobile}
+						{@html burger}
+					{:else}
+						{@html cross}
+					{/if}
+				</button>
+			{/if}
+		</div>
+		<nav style:display={showMobile || desktop ? 'grid' : 'none'}>
+			<ul style:flex-direction={desktop ? 'row' : 'column'}>
+				{#each navItems as item}
+					<li>
+						<a
+							href={item.href}
+							class:activeRoute={matchBaseRoute(
+								item.href,
+								$page.url.pathname.toString()
+							)}
+							on:click={toggleMobile}>{item.label}</a
+						>
+					</li>
+					<hr style:display={!desktop ? 'block' : 'none'} style="width: 80%" />
+				{/each}
+			</ul>
+		</nav>
 	</header>
 </div>
 
@@ -105,6 +100,12 @@
 		margin-inline: auto;
 		/* height: auto; */
 		font-family: var(--ff-circle);
+	}
+
+	.main-header {
+		display: flex;
+		justify-content: space-between;
+		width: 100%;
 	}
 
 	a {
@@ -131,16 +132,16 @@
 		color: var(--stone-900);
 	}
 
-	/* ul { */
-	/* 	gap: 1rem; */
-	/* 	display: flex; */
-	/* 	flex-direction: row; */
-	/* 	place-items: center; */
-	/* } */
+	ul {
+		gap: 0 1rem;
+		display: flex;
+		flex-direction: row;
+		place-items: center;
+	}
 
-	/* .activeRoute { */
-	/* 	text-decoration: underline; */
-	/* 	text-decoration-thickness: 0.2ch; */
-	/* 	text-decoration-color: rgb(157, 161, 180); */
-	/* } */
+	.activeRoute {
+		text-decoration: underline;
+		text-decoration-thickness: 0.2ch;
+		text-decoration-color: rgb(157, 161, 180);
+	}
 </style>
