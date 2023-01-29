1242d9b1eadd9d5225e03252058b63c6548bc37d
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-01-28 21:05:43 -0300

    Use `dialog` to create a responsive navbar at `Nav`

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -2,17 +2,25 @@
 	import { base } from '$app/paths';
 	import { page } from '$app/stores';
 
-	let y = 0;
 	export let pageHeight = 0;
+
+	let y = 0;
+	let burger =
+		'<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 48 48"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M7.95 11.95h32m-32 12h32m-32 12h32"/></svg>';
+	let cross =
+		'<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 16 16"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m11.25 4.75l-6.5 6.5m0-6.5l6.5 6.5"/></svg>';
+	let modal: HTMLDialogElement;
+
 	$: windowHeight = 0;
+	$: windowWidth = 0;
 	$: pageHeightWithoutWindow = pageHeight - windowHeight;
 
 	const navItems = [
 		{ label: 'Home', href: `${base}/` },
-		// { label: 'Projects', href: `${base}/projects` },
-		// { label: 'Blog', href: `${base}/blog` },
-		// { label: 'About', href: `${base}/about` },
-		// { label: 'Contact', href: `${base}/contact` }
+		{ label: 'Projects', href: `${base}/projects` },
+		{ label: 'Blog', href: `${base}/blog` },
+		{ label: 'About', href: `${base}/about` },
+		{ label: 'Contact', href: `${base}/contact` }
 	];
 
 	function matchBaseRoute(navItemLink: string, currentLink: string) {
@@ -28,24 +36,56 @@
 	}
 </script>
 
-<svelte:window bind:innerHeight={windowHeight} bind:scrollY={y} />
+<svelte:window
+	bind:outerWidth={windowWidth}
+	bind:innerHeight={windowHeight}
+	bind:scrollY={y}
+/>
 
 <header>
 	<a href="{base}/">Logo</a>
 	<nav>
-		<ul class="flex">
-			{#each navItems as item}
-				<li>
-					<a
-						href={item.href}
-						class:current={matchBaseRoute(
-							item.href,
-							$page.url.pathname.toString()
-						)}>{item.label}</a
-					>
-				</li>
-			{/each}
-		</ul>
+		{#if windowWidth > 700}
+			<ul>
+				{#each navItems as item}
+					<li>
+						<a
+							href={item.href}
+							class:activeRoute={matchBaseRoute(
+								item.href,
+								$page.url.pathname.toString()
+							)}>{item.label}</a
+						>
+					</li>
+				{/each}
+			</ul>
+		{:else}
+			<button
+				on:click={() => {
+					modal.showModal();
+				}}>{@html burger}</button
+			>
+			<dialog bind:this={modal}>
+				<ul>
+					{#each navItems as item}
+						<li>
+							<a
+								href={item.href}
+								class:activeRoute={matchBaseRoute(
+									item.href,
+									$page.url.pathname.toString()
+								)}>{item.label}</a
+							>
+						</li>
+					{/each}
+				</ul>
+				<button
+					on:click={() => {
+						modal.close();
+					}}>{@html cross}</button
+				>
+			</dialog>
+		{/if}
 	</nav>
 	<progress max={pageHeightWithoutWindow} value={y} />
 </header>
@@ -59,13 +99,15 @@
 		display: flex;
 		place-items: center;
 		justify-content: space-between;
-		width: min(100% - 3rem, var(--wide));
+		width: min(100% - 1rem, var(--wide));
 		margin-inline: auto;
 		/* outline: 3px red solid; */
 		font-family: var(--ff-circle);
+		/* transition: all 0.3s cubic-bezier(0.07, 0.95, 0, 1); */
 	}
 
 	ul {
+		gap: 1rem;
 		display: flex;
 		flex-direction: row;
 		place-items: center;
@@ -89,20 +131,25 @@
 		color: var(--stone-900);
 	}
 
-	.current {
+	.activeRoute {
 		text-decoration: underline;
 		text-decoration-thickness: 0.2ch;
 		text-decoration-color: rgb(157, 161, 180);
 	}
 
-	/* header > a:first-child { */
-	/* 	padding-left: 0; */
-	/* } */
+	dialog {
+		border: 0.18rem solid var(--neutral-600);
+		border-radius: 0.6rem;
+		box-shadow: 0 0 1em rgb(0 0 0 / 0.3);
+		width: 100%;
+		height: 100%;
+	}
 
-	/* ul li:last-child > a { */
-	/* 	padding-right: 0; */
-	/* 	margin-right: 1rem; */
-	/* } */
+	dialog ul {
+		place-content: center;
+		place-items: center;
+		flex-direction: column;
+	}
 
 	progress {
 		top: 0;
@@ -125,6 +172,7 @@
 	progress::-moz-progress-bar {
 		background: var(--neutral-900);
 	}
+
 	progress::-webkit-progress-value {
 		background: var(--neutral-900);
 	}
