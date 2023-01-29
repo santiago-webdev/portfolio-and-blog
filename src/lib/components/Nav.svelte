5bd1365cc15e82d6cf0fafd2d6adf6262f404c0d
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-01-29 13:45:37 -0300

    Format

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -9,7 +9,9 @@
 	let cross =
 		'<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 16 16"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m11.25 4.75l-6.5 6.5m0-6.5l6.5 6.5"/></svg>';
 	let modal: HTMLDialogElement;
-	const closeModal = () => {if (modal) modal.open ? modal.close() : null};
+	const closeModal = () => {
+		if (modal) modal.open ? modal.close() : null;
+	};
 	const openModal = () => modal.showModal();
 
 	let y = 0;
