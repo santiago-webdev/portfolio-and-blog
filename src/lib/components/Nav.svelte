42d79c86522f92b087ed8e689d2f1ca2c9ab1f79
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-01-28 21:57:49 -0300

    Use `let` instead of a reactive statement with binded values

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -5,7 +5,6 @@
 
 	export let pageHeight = 0;
 
-	let y = 0;
 	let burger =
 		'<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 48 48"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M7.95 11.95h32m-32 12h32m-32 12h32"/></svg>';
 	let cross =
@@ -14,8 +13,9 @@
 	const closeModal = () => modal.close();
 	const openModal = () => modal.showModal();
 
-	$: windowHeight = 0;
-	$: windowWidth = 0;
+	let y = 0;
+	let windowHeight = 0;
+	let windowWidth = 0;
 	$: if ($navigating || y) closeModal()
 
 	const navItems = [
