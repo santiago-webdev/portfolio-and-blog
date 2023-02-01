83fac928a30c24f604108486b0505cabdc64587a
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-02-01 19:11:57 -0300

    Fallback to "system"
    This is going to be the first value for every user that access this site
    since there's no cookie present the first time.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,7 @@
 import type { Handle } from '@sveltejs/kit';
 
 export const handle = (async ({ event, resolve }) => {
-	const theme = event.cookies.get('theme');
+	const theme = event.cookies.get('theme') || 'system';
 	const response = await resolve(event, {
 		transformPageChunk: ({ html }) =>
 			html.replace('data-theme=""', `data-theme="${theme}"`)
