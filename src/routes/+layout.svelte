cea68c276ecc559a8b5bd96f9ba10c4b6d218f55
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-02-02 11:22:20 -0300

    Remove commented code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -41,15 +41,6 @@
 </div>
 
 <svelte:head>
-	<!-- <script> -->
-	<!-- 	if ( -->
-	<!-- 		localStorage.theme === 'dark' || -->
-	<!-- 		(!localStorage.theme && -->
-	<!-- 			window.matchMedia('(prefers-color-scheme: dark)').matches) -->
-	<!-- 	) { -->
-	<!-- 		document.documentElement.dataset.theme = 'dark'; -->
-	<!-- 	} -->
-	<!-- </script> -->
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 </svelte:head>
 
