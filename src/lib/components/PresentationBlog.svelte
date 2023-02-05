2547a3abaf4d1bc663e3c7f444ee75741d1eaaef
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-02-05 11:29:56 -0300

    Basic logic for scrollable carousel

--- src/lib/components/PresentationBlog.svelte
+++ src/lib/components/PresentationBlog.svelte
@@ -2,8 +2,18 @@
 	import { base } from '$app/paths';
 	import { onMount } from 'svelte';
 
+	const blogCards = [
+		{ title: 'Title 1', description: 'Description 1' },
+		{ title: 'Title 2', description: 'Description 2' },
+		{ title: 'Title 3', description: 'Description 3' },
+		{ title: 'Title 4', description: 'Description 4' },
+		{ title: 'Title 5', description: 'Description 5' },
+		{ title: 'Title 6', description: 'Description 6' }
+	];
+
 	let blog: HTMLElement;
 	let blogButton: HTMLElement;
+	let wrapperCards: HTMLElement;
 	onMount(async () => {
 		let loadBlog: IntersectionObserver = new IntersectionObserver(entries => {
 			entries.some(entry => {
@@ -16,19 +26,36 @@
 
 		loadBlog.observe(blog);
 	});
+
+	let windowWidth: number;
+	function gotoCard(direction: string) {
+		if (direction === 'next') {
+			wrapperCards.scrollBy({ top: 0, left: windowWidth / 2, behavior: 'smooth', })
+		} else if (direction === 'previous') {
+			wrapperCards.scrollBy({ top: 0, left: -Math.abs(windowWidth / 2), behavior: 'smooth', })
+		}
+	}
 </script>
 
-<section class="focus" id="read-my-blog">
-	<div class="wrapper-cards">
-		<div style="background-color: blue" class="hero-blog">Cards go here</div>
-		<div style="background-color: red" class="hero-blog">Cards go here</div>
-		<div style="background-color: green" class="hero-blog">Cards go here</div>
-		<div style="background-color: yellow" class="hero-blog">Cards go here</div>
+<svelte:window bind:outerWidth={windowWidth} />
+
+<section class="focus wide" id="read-my-blog">
+	<div class="wrapper-cards" bind:this={wrapperCards}>
+		{#each blogCards as blog, i}
+			<article class="blog-selection" id={`./#recommendation-${i}`}>
+				<a href={`./#recommendation-${i}`}>
+					{blog.title}
+					{blog.description}
+				</a>
+			</article>
+		{/each}
 	</div>
 	<div bind:this={blog} class="wrapper">
 		<h2>Read my blog</h2>
 		<p>I don't just focus in one topic, so here's a some articles</p>
 		<a bind:this={blogButton} href="{base}/blog">Go read the blog</a>
+		<button on:click={() => gotoCard('previous')}>Previous</button>
+		<button on:click={() => gotoCard('next')}>Next</button>
 	</div>
 </section>
 
@@ -41,7 +68,7 @@
 		text-align: right;
 	}
 
-	.hero-blog {
+	.blog-selection {
 		background-color: red;
 
 		width: 100%;
@@ -52,6 +79,8 @@
 
 		scroll-snap-align: center;
 		scroll-snap-type: x mandatory;
+
+		text-align: center;
 	}
 
 	.wrapper-cards {
@@ -63,12 +92,12 @@
 	}
 
 	/* This removes the scrollbar */
-	.wrapper-cards {
-		overflow-x: scroll;
-		scrollbar-width: none;
-	}
+	/* .wrapper-cards { */
+	/* 	overflow-x: scroll; */
+	/* 	scrollbar-width: none; */
+	/* } */
 
-	.wrapper-cards::-webkit-scrollbar {
-		display: none;
-	}
+	/* .wrapper-cards::-webkit-scrollbar { */
+	/* 	display: none; */
+	/* } */
 </style>
