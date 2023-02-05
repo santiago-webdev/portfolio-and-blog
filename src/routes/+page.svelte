002c4ad915e596335d84bba8929ba891adfae77b
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-02-05 19:47:40 -0300

    Lock/snap when scrolling in the entire `/` page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,52 @@
 	import Presentation from '$lib/components/Presentation.svelte';
 	import PresentationBlog from '$lib/components/PresentationBlog.svelte';
 	import { DESCRIPTION, TITLE, URL } from '$lib/config';
+	import { onMount } from 'svelte';
+
+	let currentIndex = 0;
+	let main: HTMLElement;
+	let section1: HTMLElement;
+
+	let sections: Array<HTMLElement>
+
+	onMount(() => {
+		const observer = new IntersectionObserver(entries => {
+			entries.forEach(entry => {
+				if (entry.isIntersecting && entry.target.parentNode) {
+					currentIndex = Array.from(entry.target.parentNode.children).indexOf(
+						entry.target
+					);
+				}
+			});
+		});
+
+		// sections = document.querySelectorAll('main, #section1');
+		sections = [
+			main,
+			section1
+		]
+
+		sections.forEach((section: any) => {
+			observer.observe(section);
+		});
+
+		window.addEventListener('wheel', event => {
+			if (event.deltaY > 0) {
+				currentIndex = Math.min(currentIndex + 1, sections.length - 1);
+			} else {
+				currentIndex = Math.max(currentIndex - 1, 0);
+			}
+
+			document.body.scrollTop = sections[currentIndex].offsetTop;
+			document.documentElement.scrollTop = sections[currentIndex].offsetTop;
+		});
+	});
 </script>
 
-<main>
+<main bind:this={main}>
 	<Presentation />
 </main>
-<section>
+<section bind:this={section1}>
 	<PresentationBlog />
 </section>
 
