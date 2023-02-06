baff9611dd14776d43a209e95b670696af9de234
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-02-06 19:33:43 -0300

    WIP: Change "search" button for icon button

--- src/lib/components/FilterFunctionality/SearchBar.svelte
+++ src/lib/components/FilterFunctionality/SearchBar.svelte
@@ -15,7 +15,7 @@
 	}
 
 	const search_icon =
-		'<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 1200 1200"><path fill="currentColor" d="M672.633 0C389.326 0 159.634 229.652 159.634 512.958c0 100.662 28.986 194.563 79.083 273.79c-.368.16-.729.305-1.098.471l-223.21 223.172L204.019 1200l231.249-231.288c-.069-.326-.162-.651-.234-.979c71.035 37.19 151.865 58.224 237.6 58.224c283.309 0 512.959-229.69 512.959-512.997C1185.59 229.652 955.939 0 672.633 0zm0 227.877c157.441 0 285.041 127.639 285.041 285.081s-127.6 285.081-285.041 285.081c-157.442 0-285.082-127.639-285.082-285.081S515.19 227.877 672.633 227.877z"/></svg>';
+		'<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32"><path fill="currentColor" d="m29 27.586l-7.552-7.552a11.018 11.018 0 1 0-1.414 1.414L27.586 29ZM4 13a9 9 0 1 1 9 9a9.01 9.01 0 0 1-9-9Z"></path></svg>';
 	const placeholderDefault = 'Search';
 	const placeholderNoInput = "You haven't searched for any post yet";
 	let placeholder = placeholderDefault;
@@ -50,15 +50,14 @@
 			<label for="search">Search for a blog post</label>
 			<div class="search-bar">
 				<input
+					{placeholder}
 					bind:this={input}
 					bind:value
 					type="search"
 					id="search"
 					list="search-terms"
 				/>
-				<button aria-label="Navigate to selected blog article" type="submit"
-					>Read</button
-				>
+				<button aria-label="Navigate to selected blog article" type="submit" > {@html search_icon} </button>
 			</div>
 		</form>
 	</div>
@@ -104,11 +103,14 @@
 		width: 100%;
 		margin-inline: auto;
 		color: var(--clr-subtitle);
-		/* outline: 3px red solid; */
 	}
 
 	label {
 		text-indent: 0.8rem;
+	}
+
+	.search-bar {
+		background-color: red;
 	}
 
 	input {
@@ -116,7 +118,6 @@
 		border-radius: 0.4rem;
 		padding: 0.3rem;
 		text-indent: 0.3rem;
-		background-color: crimson;
 		width: 100%;
 	}
 
@@ -124,10 +125,10 @@
 		color: transparent;
 	}
 
-	input:focus,
-	button {
-		outline: 0.13rem solid blue;
-	}
+	/* input:focus, */
+	/* button { */
+	/* 	outline: 0.13rem solid blue; */
+	/* } */
 
 	.search-bar {
 		display: flex;
@@ -136,7 +137,8 @@
 	}
 
 	button {
-		background-color: lightgray;
+		display: grid;
+		place-items: center;
 		border-radius: 0.4rem;
 		padding-inline: 1.5rem;
 	}
