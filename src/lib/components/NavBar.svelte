f326ed4de7d58490ff4d9ff983d8514c8c78aead
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-02-07 20:28:59 -0300

    Add same shadow that is being used for active state of search bar

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -142,6 +142,8 @@
 
 	.headerActive {
 		background-color: var(--clr-background);
+		filter: drop-shadow(0 10px 8px rgb(0 0 0 / 0.04))
+			drop-shadow(0 4px 3px rgb(0 0 0 / 0.1));
 	}
 
 	.header-container {
