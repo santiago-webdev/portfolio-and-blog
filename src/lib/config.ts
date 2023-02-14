1545641259a26e89d5d843c0d35e6288b06d50a4
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-02-14 06:28:14 -0300

    Add meta tags /blog

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -5,6 +5,7 @@ export const DESCRIPTION =
   'Personal blog and portfolio, I talk about web development and show you my projects while at it.';
 
 export const URL = 'https://santigo-zero.github.io/';
+export const URL_BLOG = 'https://santigo-zero.github.io/blog';
 export const REPO = 'https://github.com/santigo-zero/santigo-zero.github.io';
 export const HOST = 'https://github.com/santigo-zero/santigo-zero.github.io';
 
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import SearchBar from '$lib/components/Search/SearchBar.svelte';
   import FilteredPostsCards from '$lib/components/Search/FilteredPostsCards.svelte';
-  import { TITLE_BLOG } from '$lib/config';
+  import { DESCRIPTION, TITLE_BLOG, URL_BLOG } from '$lib/config';
   import '@fontsource/pt-sans/400.css';
   import '@fontsource/pt-sans/700.css';
 </script>
@@ -13,6 +13,29 @@
 
 <svelte:head>
   <title>{TITLE_BLOG}</title>
-</svelte:head>
 
-<!-- TODO(santigo-zero): Add meta tags -->
+  <!-- TODO(santigo-zero): meta tags for dynamic routing -->
+  <!-- HTML Meta Tags -->
+  <meta name="description" content={DESCRIPTION} />
+
+  <!-- Google / Search Engine Tags -->
+  <meta itemprop="name" content={TITLE_BLOG} />
+  <meta itemprop="description" content={DESCRIPTION} />
+  <meta itemprop="image" content="" />
+  <!-- TODO(santigo-zero): Add image -->
+
+  <!-- Facebook Meta Tags -->
+  <meta property="og:url" content={URL_BLOG} />
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content={TITLE_BLOG} />
+  <meta property="og:description" content={DESCRIPTION} />
+  <meta property="og:image" content="" />
+  <!-- TODO(santigo-zero): Add image -->
+
+  <!-- Twitter Meta Tags -->
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:title" content={TITLE_BLOG} />
+  <meta name="twitter:description" content={DESCRIPTION} />
+  <meta name="twitter:image" content="" />
+  <!-- TODO(santigo-zero): Add image -->
+</svelte:head>
