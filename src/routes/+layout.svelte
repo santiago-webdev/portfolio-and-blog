df47d5c9bf45a615e9b8b4ae5d8d4e42c8764be3
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-02-18 14:51:01 -0300

    Remove fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,13 @@
   import '@fontsource/pt-sans/700.css';
   import '@fontsource/pt-sans-narrow/400.css';
   import '@fontsource/pt-sans-narrow/700.css';
+  import "@fontsource/manrope/200.css"
+  import "@fontsource/manrope/300.css"
+  import "@fontsource/manrope/400.css"
+  import "@fontsource/manrope/500.css"
+  import "@fontsource/manrope/600.css"
+  import "@fontsource/manrope/700.css"
+  import "@fontsource/manrope/800.css"
 
   let body: HTMLElement;
   $: pageHeight = 0;
