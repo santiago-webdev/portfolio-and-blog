7c9266bd32d4eb4d5975cb91a9fd1c07a3ef1741
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-02-19 11:27:59 -0300

    Use hooks.server.ts to set the theme before serving the page

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,15 +1,10 @@
-// import type { Handle } from '@sveltejs/kit';
+import type { Handle } from '@sveltejs/kit';
 
-// export const handle = (async ({ event, resolve }) => {
-// 	const theme = event.cookies.get('theme') || 'system';
-// 	const response = await resolve(event, {
-// 		transformPageChunk: ({ html }) =>
-// 			html.replace('data-theme=""', `data-theme="${theme}"`)
-// 	});
-// 	return response;
-// }) satisfies Handle;
-
-// Since this is an SSG site, not an SSR site, we have to rely on changes to the <head> of
-// each route instead of using this simpler logic.
-
-export {};
+export const handle = (async ({ event, resolve }) => {
+	const theme = event.cookies.get('theme') || 'system';
+	const response = await resolve(event, {
+		transformPageChunk: ({ html }) =>
+			html.replace('data-theme=""', `data-theme="${theme}"`)
+	});
+	return response;
+}) satisfies Handle;
