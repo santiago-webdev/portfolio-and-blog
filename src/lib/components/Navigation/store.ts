c3aa0dcf1d09728c800b843c41d013c1ab669266
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-02-21 20:33:47 -0300

    Format

--- src/lib/components/Navigation/store.ts
+++ src/lib/components/Navigation/store.ts
@@ -1,13 +1,13 @@
-import { base } from '$app/paths'
-import { writable } from 'svelte/store'
+import { base } from '$app/paths';
+import { writable } from 'svelte/store';
 
 type NavItems = Array<{
-  label?: string
-  href?: string
-  decoration?: string
-  separator?: string
-}>
+  label?: string;
+  href?: string;
+  decoration?: string;
+  separator?: string;
+}>;
 
 export const navStore = writable<NavItems>([
   { label: 'Home', href: `${base}/` }
-])
+]);
