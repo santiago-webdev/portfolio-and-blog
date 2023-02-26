a46cd3a216381d34a63cfdd5ff27e27c05e045e2
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-02-25 21:36:57 -0300

    Build
    Center icon and use aria-expanded for button

---
+++ .vercel/output/static/_app/immutable/chunks/2-03a883eb.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-1260c0aa.js";export{t as component};
--- .vercel/output/static/_app/immutable/chunks/2-5c43c009.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-ee990ce8.js";export{t as component};
