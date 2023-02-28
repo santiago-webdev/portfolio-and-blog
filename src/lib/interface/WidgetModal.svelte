60a4329fbf4962f2bb21fcb4a51da139a28aabeb
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-02-28 19:07:47 -0300

    Conditional for modal box widget
    The search icon design will be used only if the device has touch support
    and is in a small screen, else we will show the more complex
    design of the widget with the keyboard shortcut being shown.

--- src/lib/interface/WidgetModal.svelte
+++ src/lib/interface/WidgetModal.svelte
@@ -1,22 +1,28 @@
 <script lang="ts">
   import IconSearch from '$lib/Icons/IconSearch.svelte';
   import { inPixels } from '$lib/utils/utils';
+  import { onMount } from 'svelte';
 
   var onDesktop = true,
-    outerWidth = 0;
+    outerWidth = 0,
+    complex = true,
+    touchSupport = false;
+
+  onMount(() => (touchSupport = 'ontouchstart' in window ? true : false));
 
   $: onDesktop = outerWidth > inPixels('48rem') ? true : false;
+  $: complex = onDesktop || !touchSupport;
 </script>
 
 <svelte:window bind:outerWidth />
 
 <button
   on:click={() => console.log('test')}
-  class={onDesktop ? 'attn attnactive' : ''}
+  class={complex ? 'attn attnactive' : ''}
   aria-keyshortcuts="Control+K"
 >
   <IconSearch />
-  {#if onDesktop}
+  {#if complex}
     <span> &nbsp;&nbsp;&nbsp </span>
     <kbd>Ctrl K</kbd>
   {/if}
@@ -42,6 +48,6 @@
     border-right: 3px solid var(--clr-text-muted);
     border-bottom: 3px solid var(--clr-text-muted);
     border-radius: 0.4rem;
-    /* padding-inline: 0.3rem; */
+    padding-inline: 0.3rem;
   }
 </style>
