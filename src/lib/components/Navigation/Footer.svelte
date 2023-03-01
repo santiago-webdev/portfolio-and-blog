9ff0b53b51ff484d1dec0ffb5ab6c0cd54881e1e
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-03-01 20:16:12 -0300

    WIP: New look for `Footer`

--- src/lib/components/Navigation/Footer.svelte
+++ src/lib/components/Navigation/Footer.svelte
@@ -22,49 +22,100 @@
   getGithubStars().then(stars => (ghStars = stars));
 </script>
 
-<footer class="banner-reverse artifact-ui">
-  <section class="upper wider">
-    <small
-      >Designed & Built with <iconify-icon icon="lucide:heart" /> by Santiago</small
-    >
-    <a
-      href="https://github.com/santigo-zero/santigo-zero.github.io"
-      aria-label="Link to git repository for this site at Github"
-      >Repository at<iconify-icon icon="lucide:github" />-
-      <iconify-icon icon="lucide:star" />
-      <span>
-        {ghStars}
-      </span>
-    </a>
-  </section>
-  <section class="bottom wider">
-    <div class="copyright">
-      <small>© GPL-3.0 Santiago Gonzalez</small>
-      <small>2023. All Rights Reserved</small>
+<footer aria-labelledby="main-footer" class="banner-reverse artifact-ui">
+  <div id="main-footer">
+    <section class="upper wider">
+      <div class="services">
+        My services
+        <ol>
+          <li>Web Design</li>
+        </ol>
+      </div>
+      <ul>
+        <!-- TODO(santigo-zero): Move all of this to #cta -->
+        <li>
+          <a
+            href="https://github.com/santigo-zero/"
+            aria-label="Link to my GitHub profile">GitHub</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://github.com/santigo-zero/"
+            aria-label="Link to my GitHub profile">GitHub</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://github.com/santigo-zero/"
+            aria-label="Link to my GitHub profile">GitHub</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://github.com/santigo-zero/"
+            aria-label="Link to my GitHub profile">GitHub</a
+          >
+        </li>
+        <!-- <li class="separator">•</li> -->
+        <li>
+          <a
+            href="https://www.linkedin.com/in/santiago-gonzalez-62557221b/"
+            aria-label="Link to my LinkedIn profile"
+          >
+            LinkedIn</a
+          >
+        </li>
+      </ul>
+      <div class="aboutme">
+        <h3>Santiago Gonzalez</h3>
+        <h3>
+          I'm a web developer with a purpose, bring accessibility to the web.
+        </h3>
+      </div>
+    </section>
+    <div class="wrapper-bottom">
+      <section class="bottom wider">
+        <div class="copyright">
+          <small
+            >© GPL-3.0 2023
+            <span class="separator">•</span>
+            All Rights Reserved.</small
+          >
+        </div>
+        <small
+          >Designed & Built with <iconify-icon icon="lucide:heart" /> by Santiago</small
+        >
+        <a
+          href="https://github.com/santigo-zero/santigo-zero.github.io"
+          aria-label="Link to git repository for this site at Github"
+          >Repository at<iconify-icon icon="lucide:github" />-
+          <iconify-icon icon="lucide:star" />
+          <span>
+            {ghStars}
+          </span>
+        </a>
+      </section>
     </div>
-    <ul>
-      <!-- TODO(santigo-zero): Move all of this to #cta -->
-      <li>
-        <a
-          href="https://github.com/santigo-zero/"
-          aria-label="Link to my GitHub profile">GitHub @santigo-zero</a
-        >
-      </li>
-      <li class="separator">•</li>
-      <li>
-        <a
-          href="https://www.linkedin.com/in/santiago-gonzalez-62557221b/"
-          aria-label="Link to my LinkedIn profile"
-        >
-          LinkedIn</a
-        >
-      </li>
-      <!-- TODO(santigo-zero): Add my email here -->
-    </ul>
-  </section>
+  </div>
 </footer>
 
 <style>
+  footer {
+    min-height: min(50vh, calc(var(--wide) / (16 / 9)));
+    padding-bottom: 0;
+  }
+
+  #main-footer {
+    display: grid;
+    grid-template-rows: 1fr auto;
+  }
+
+  h3:last-child {
+    font-weight: lighter;
+    color: var(--clr-text-muted);
+  }
+
   a {
     display: flex;
     place-items: center;
@@ -91,13 +142,30 @@
   ul {
     display: flex;
     place-items: center;
-    flex-direction: row;
-    gap: 0.6rem;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: inherit;
+    gap: 1.5rem;
+  }
+
+  .wrapper-bottom {
+    width: 100%;
+    background-color: var(--clr-background-alt-alt);
   }
 
   .bottom {
+    display: flex;
     flex-wrap: wrap;
-    gap: 1rem;
+    gap: 1.5rem;
+    padding: 1.5rem 0;
+    color: var(--clr-text-muted);
+  }
+
+  .upper {
+    display: grid;
+    gap: 3rem;
+    grid-template-columns: repeat(auto-fill, minmax(min(30ch, 100%), 1fr));
+    padding: 1rem;
   }
 
   .copyright {
@@ -114,19 +182,12 @@
   }
 
   @media screen and (max-width: 48rem) {
-    .upper,
-    .bottom {
-      flex-direction: column-reverse;
-      gap: 1rem;
-      text-align: center;
-    }
-
     .separator {
       display: none;
     }
 
-    ul {
-      flex-direction: column;
+    .bottom {
+      justify-content: space-around;
     }
   }
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -36,7 +36,7 @@
     min-height: 100vh;
     min-height: 100dvh;
     display: grid;
-    grid-template-rows: auto 1fr auto;
+    grid-template-rows: 1fr auto;
     background-color: var(--clr-background);
   }
 </style>
