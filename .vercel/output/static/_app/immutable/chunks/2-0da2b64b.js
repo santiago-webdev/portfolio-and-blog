6b197b03bd0e3471587cdb50d745fa6ed6a2cc0f
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-03-01 20:36:22 -0300

    Build
    Make `NavBar` show when reaching the bottom of the page

---
+++ .vercel/output/static/_app/immutable/chunks/2-0da2b64b.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-a96a6b6c.js";export{t as component};
--- .vercel/output/static/_app/immutable/chunks/2-a2b0b46d.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-198d276e.js";export{t as component};
