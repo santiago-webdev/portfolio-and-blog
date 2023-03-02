1264afedeb1676a33c7ef62091888c0df9251c1a
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-03-02 15:12:50 -0300

    Fix padding in .banner for mobile in /blog

---
+++ .vercel/output/static/_app/immutable/chunks/4-42f28b08.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/blog/_page.svelte-f86721c1.js";export{t as component};
--- .vercel/output/static/_app/immutable/chunks/4-861ee9b7.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/blog/_page.svelte-a791c230.js";export{t as component};
