c00a1b924f75da6c27bf5af4c0c38a5999446d08
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-03-14 13:03:57 -0300

    Format

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,17 +1,17 @@
-import type { Handle } from '@sveltejs/kit'
+import type { Handle } from '@sveltejs/kit';
 
-type Theme = 'light' | 'dark' | 'system'
+type Theme = 'light' | 'dark' | 'system';
 
 export const handle = (async ({ event, resolve }) => {
-  const get_theme = event.cookies.get('theme')
-  let theme: Theme =
-    get_theme === 'light' || get_theme === 'dark' ? get_theme : 'system'
+  const get_theme = event.cookies.get('theme');
+  const theme: Theme =
+    get_theme === 'light' || get_theme === 'dark' ? get_theme : 'system';
 
   return theme === 'system'
     ? await resolve(event)
     : await resolve(event, {
         transformPageChunk: ({ html }) => {
-          return html.replace('data-theme="system"', `data-theme="${theme}"`)
+          return html.replace('data-theme="system"', `data-theme="${theme}"`);
         }
-      })
-}) satisfies Handle
+      });
+}) satisfies Handle;
