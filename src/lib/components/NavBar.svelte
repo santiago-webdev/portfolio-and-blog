75e85b60f327ededc8a896fb5540f8345289a545
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-03-15 17:35:43 -0300

    Added X in `NavBar` navigation

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -159,7 +159,7 @@
         on:click={() => (expanded = !expanded)}
         style:display={currentContext.length > 0 ? '' : 'none'}
         bind:this={button}>
-        <iconify-icon width="24" icon={currentContext} />
+        <iconify-icon width="24" icon={expanded ? 'lucide:x' : currentContext} />
       </button>
     </div>
   </div>
