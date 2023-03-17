0c426b557bce9ce3569d2b3082b3515dc5f1af13
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-03-17 18:11:53 -0300

    Make separated component for arrow animation

---
+++ src/lib/components/ChevronToArrow.svelte
@@ -0,0 +1,66 @@
+<div class="wrapper-arrow">
+  <svg
+    id="arrow-tail"
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24">
+    <path
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M5 12h14" />
+  </svg>
+
+  <svg
+    id="arrow-head"
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24">
+    <path
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="m9 18l6-6l-6-6" />
+  </svg>
+</div>
+
+<style>
+  .wrapper-arrow {
+    display: flex;
+    place-items: center;
+    position: relative;
+    /* top: 2px; */
+  }
+
+  svg {
+    position: absolute;
+    transition: transform 150ms cubic-bezier(0.215, 0.61, 0.355, 1),
+      opacity 150ms cubic-bezier(0.215, 0.61, 0.355, 1);
+  }
+
+  #arrow-head {
+    transform: translateX(-3px);
+  }
+
+  #arrow-tail {
+    opacity: 0;
+    transition-delay: -50ms;
+  }
+
+  @media (hover: hover) {
+    :global(a:hover #arrow-head) {
+      transform: translateX(6px);
+    }
+
+    :global(a:hover #arrow-tail) {
+      opacity: 1;
+      transition-delay: 50ms;
+    }
+  }
+</style>
--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { base } from '$app/paths'
+  import ChevronToArrow from '$compose/ChevronToArrow.svelte'
 </script>
 
 <div class="top-layer">
@@ -17,41 +18,15 @@
         href="{base}/projects"
         aria-label="Link to my projects page"
         class="shiny hover"
-        >Projects <iconify-icon icon="lucide:chevron-right" /></a>
+        >Projects
+        <ChevronToArrow />
+      </a>
 
-      <a href="/" aria-label="Link to contact me" class="block hover"
+      <a
+        aria-label="Link to contact me"
+        class="block hover"
+        href="{base}/contact"
         >Contact
-        <div class="wrapper-arrow">
-          <svg
-            id="arrow-tail"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24">
-            <path
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M5 12h14" />
-          </svg>
-
-          <svg
-            id="arrow-head"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24">
-            <path
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="m9 18l6-6l-6-6" />
-          </svg>
-        </div>
       </a>
     </section>
   </main>
@@ -83,39 +58,6 @@
     padding: 0.8rem;
     font-size: 140%;
     gap: 0.2rem;
-  }
-
-  .wrapper-arrow {
-    display: flex;
-    place-items: center;
-    position: relative;
-    top: 1px;
-  }
-
-  svg {
-    position: absolute;
-    transition: transform 150ms cubic-bezier(0.215, 0.61, 0.355, 1),
-      opacity 150ms cubic-bezier(0.215, 0.61, 0.355, 1);
-  }
-
-  #arrow-head {
-    transform: translateX(-3px);
-  }
-
-  #arrow-tail {
-    opacity: 0;
-    transition-delay: -50ms;
-  }
-
-  @media (hover: hover) {
-    a:hover #arrow-head {
-      transform: translateX(6px);
-    }
-
-    a:hover #arrow-tail {
-      opacity: 1;
-      transition-delay: 50ms;
-    }
   }
 
   @media screen and (min-width: 68rem) {
