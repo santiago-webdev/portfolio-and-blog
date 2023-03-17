4a45dfc6984df1b6db8ee8f2855c15a2cda91405
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-03-17 08:25:00 -0300

    Transitions will only be applied in big screens

--- src/lib/components/Transition.svelte
+++ src/lib/components/Transition.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
+  import { onMount } from 'svelte'
   import { fly } from 'svelte/transition'
   export let url: URL
+
+  let client_big_screen = false
+
+  onMount(() => {
+    const bigger_than_48 = window.matchMedia('(min-width: 48rem)')
+    client_big_screen = bigger_than_48.matches
+    bigger_than_48.addEventListener(
+      'change',
+      (event: MediaQueryListEvent) => (client_big_screen = event.matches)
+    )
+  })
 </script>
 
-{#key url}
-  <div in:fly={{ y: -60, duration: 250 }}>
-    <slot />
-  </div>
-{/key}
+{#if client_big_screen}
+  {#key url}
+    <div in:fly={{ y: -60, duration: 250 }}>
+      <slot />
+    </div>
+  {/key}
+{:else}
+  <slot />
+{/if}
