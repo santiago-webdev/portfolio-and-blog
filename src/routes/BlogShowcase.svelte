e11807b2634a77820d6781d3ccbd6570fb5deaa9
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-03-19 15:55:08 -0300

    Use local variable to get latest post and remove commented code

--- src/routes/BlogShowcase.svelte
+++ src/routes/BlogShowcase.svelte
@@ -2,62 +2,9 @@
   import { base } from '$app/paths'
   import ChevronToArrow from '$compose/ChevronToArrow.svelte'
   import { Posts } from '$compose/Search/store'
-</script>
 
-<!-- <script lang="ts"> -->
-<!--   // <script context="module" lang="ts"> -->
-<!-- </script> -->
-<!---->
-<!-- <div class="focus wider" id="blog"> -->
-<!--   <a href="#blog">test</a> -->
-<!--   <h2>Check out my blog</h2> -->
-<!--   <p> -->
-<!--     I don't focus on just one subject, so here's a recommendation of a few of my -->
-<!--     posts -->
-<!--   </p> -->
-<!--   <section> -->
-<!--     <article class="shiny less attn"> -->
-<!--       <h3>Title</h3> -->
-<!--       <p>Description</p> -->
-<!--     </article> -->
-<!---->
-<!--     <article class="shiny less attn"> -->
-<!--       <h3>Title</h3> -->
-<!--       <p>Description</p> -->
-<!--     </article> -->
-<!---->
-<!--     <article class="shiny less attn"> -->
-<!--       <h3>Title</h3> -->
-<!--       <p>Description</p> -->
-<!--     </article> -->
-<!--   </section> -->
-<!-- </div> -->
-<!---->
-<!-- <style> -->
-<!--   #blog { -->
-<!--     display: flex; -->
-<!--     flex-direction: column; -->
-<!--     gap: 2rem; -->
-<!---->
-<!--     text-align: center; -->
-<!--     padding: 3rem 0; -->
-<!--     /* width: 100%; */ -->
-<!--   } -->
-<!---->
-<!--   p { -->
-<!--     max-width: 40ch; -->
-<!--     margin-inline: auto; -->
-<!--   } -->
-<!---->
-<!--   section { -->
-<!--     display: grid; -->
-<!--     place-items: center; -->
-<!--     justify-content: center; -->
-<!--     grid-template-columns: repeat(auto-fill, minmax(min(22rem, 100%), 1fr)); -->
-<!--     width: 100%; -->
-<!--     gap: 1rem; -->
-<!--   } -->
-<!-- </style> -->
+  let latestPost = $Posts[0]
+</script>
 
 <section class="wider" id="blog">
   <div class="present">
@@ -72,10 +19,10 @@
     </a>
   </div>
 
-  <a href="{base}/blog{$Posts[0].href}">
+  <a href="{base}/blog{latestPost.href}">
     <article class="shiny block">
-      <h3>{$Posts[0].title}</h3>
-      <p>{$Posts[0].description}</p>
+      <h3>{latestPost.title}</h3>
+      <p>{latestPost.description}</p>
     </article>
   </a>
 
