c473e1fabb37ae07624c11d0ff31ecdf1a5f6062
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-03-25 22:45:54 -0300

    Scroll back to top button appears after scrolling

--- src/lib/components/ScrollBackToTop.svelte
+++ src/lib/components/ScrollBackToTop.svelte
@@ -1,15 +1,16 @@
-<script>
-  import { onMount } from 'svelte'
+<script lang="ts">
+  var js_is_enabled = true,
+    scrollY = 0,
+    outerHeight = 0
 
-  let js_is_enabled = false
-
-  onMount(() => {
-    js_is_enabled = true
-  })
+  $: is_past_half_screen = scrollY > outerHeight / 2
 </script>
+
+<svelte:window bind:scrollY bind:outerHeight />
 
 {#if js_is_enabled}
   <button
+    style:display={is_past_half_screen ? 'flex' : 'none'}
     aria-label="Scroll to the top of the page"
     title="Scroll to the top of the page"
     on:click={() =>
@@ -17,29 +18,26 @@
         top: 0,
         behavior: 'smooth',
       })}>
-    <iconify-icon width="28" icon="lucide:chevron-up" />
+    <iconify-icon width="22" icon="lucide:arrow-up" /> Scroll to top
   </button>
 {:else}
-  <a href="#body">Scroll to Top</a>
+  <a href="#body">Scroll to top</a>
 {/if}
 
 <style>
   button,
   a {
-    display: grid;
+    align-items: center;
+    gap: 0.4rem;
     position: fixed;
     bottom: 1.5rem;
     right: 1.5rem;
     z-index: 999;
     background-color: var(--accent-peachy);
-    opacity: 0.6;
+    opacity: 0.8;
     color: var(--clr-bg-400);
-    padding: 0.8rem;
-    border-radius: 50%;
-  }
-
-  a {
-    border-radius: 1rem;
+    padding: 0.6rem;
+    border-radius: 0.8rem;
     font-weight: bolder;
   }
 </style>
