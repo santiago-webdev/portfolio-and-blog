c26361442d9cf76003c1479cf5d559fe46b1bd20
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-04-03 19:17:18 -0300

    Add default font, breakpoints, smooth scrolling
    And new formatter settings

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,25 @@
 <script lang="ts">
-	import '$lib/styles/reset.css';
+  import '$lib/styles/reset.css'
 </script>
 
 <slot />
 
 <style global>
-	html {
-		scroll-behavior: smooth;
-	}
+  :root {
+    --ff-dflt: 'Manrope', system-ui, -apple-system, BlinkMacSystemFont,
+      'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji',
+      'Segoe UI Emoji', 'Segoe UI Symbol';
+
+    /* Breakpoints */
+    --xs: 20rem; /* 320px */
+    --sm: 48rem; /* 768px */
+    --base: 64rem; /* 1024px Prose */
+    --md: 85.375rem; /* 1366px */
+    --lg: 90rem; /* 1440px */
+    --xl: 120rem; /* 1920px */
+  }
+
+  html {
+    scroll-behavior: smooth;
+  }
 </style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
-import adapter from '@sveltejs/adapter-vercel';
-import sveltePreprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-vercel'
+import sveltePreprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: sveltePreprocess(),
+  preprocess: sveltePreprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
-};
+  kit: {
+    adapter: adapter(),
+  },
+}
 
-export default config;
+export default config
