7ad7703bc4641c054ad23fbee6c084c837d29e41
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-04-05 20:43:26 -0300

    Small changes to layout and styles for "about" introduction

--- src/routes/(root)/IntroductionAbout.svelte
+++ src/routes/(root)/IntroductionAbout.svelte
@@ -4,20 +4,23 @@
 </script>
 
 <div id="introduction-about-me" class="about-me">
-  <div class="introduction-about-me">
-    <a href="#introduction-about-me">
-      <h2>About</h2>
-    </a>
-    <h3 style="color: inherit" class="ff-sz-800">Hello!</h3>
-    <p>
-      I'm Santiago Gonzalez, a web developer based in Argentina and I'm trying
-      to make my way into the IT
-    </p>
+  <section>
+    <div>
+      <a href="#introduction-about-me">
+        <h2>About</h2>
+      </a>
+      <h3 style="color: inherit" class="ff-sz-800">Hello!</h3>
+      <p>
+        I'm Santiago Gonzalez, a web developer based in Argentina, I'm
+        interested in Linux, UI/UX and Open Source.
+      </p>
+    </div>
 
     <a class="reverse ff-sz-700" href="{base}/about"
-      >Learn more <ChevronToLeft />
+      >Learn more
+      <ChevronToLeft />
     </a>
-  </div>
+  </section>
   <div class="about-me-hero" />
 </div>
 
@@ -27,7 +30,7 @@
     margin: 1.5rem auto;
     display: grid;
     grid-template-columns: 1fr;
-    background-color: var(--bg-401);
+    background-color: var(--bg-400);
     padding: 2rem;
     border-radius: 1.2rem;
     gap: 1rem;
@@ -47,11 +50,14 @@
     gap: 0.2rem;
     border-radius: 999rem;
     font-size: 98%;
+
+    font-variation-settings: 'wght' 550;
   }
 
-  .introduction-about-me {
+  section {
     display: flex;
     flex-flow: column wrap;
+    justify-content: space-between;
     gap: 1rem;
   }
 
