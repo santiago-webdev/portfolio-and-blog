7b2be8ffe8e1927fff1e6c05443be48ed37b815f
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-04-05 06:38:47 -0300

    Added +error.svelte

---
+++ src/routes/+error.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import { base } from '$app/paths'
+  import { page } from '$app/stores'
+</script>
+
+<section>
+  <h1>404 {$page.error?.message}</h1>
+  <a class='shiny hover ff-sz-700' href="{base}/">Home</a>
+</section>
+
+<style>
+  section {
+    display: grid;
+    margin: auto;
+    gap: 2rem;
+  }
+
+  a {
+    display: flex;
+    place-items: center;
+    place-content: center;
+    padding: clamp(1rem, 1.5vw, 2rem) clamp(2rem, 3vw, 3rem);
+    gap: 0.2rem;
+    border-radius: 999rem;
+  }
+</style>
