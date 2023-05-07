dc6d3b336797d3689987d48e74178eb49bebefe4
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-05-07 11:22:42 -0300

    Try use svgs everywhere in footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -95,7 +95,24 @@
                     .then(() => console.log('Text copied to clipboard'))
                     .catch(error =>
                       console.error('Could not copy text: ', error)
-                    )}><iconify-icon icon="lucide:clipboard-copy" /></button>
+                    )}
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24">
+                  <g
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2">
+                    <rect width="8" height="4" x="8" y="2" rx="1" ry="1" />
+                    <path
+                      d="M8 4H6a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-2M16 4h2a2 2 0 0 1 2 2v4m1 4H11" />
+                    <path d="m15 10l-4 4l4 4" />
+                  </g>
+                </svg></button>
             </li>
           {/each}
         </ul>
@@ -142,7 +159,19 @@
         aria-label="Link to my GitHub profile"
         title="Link to my GitHub profile">
         <small class="font-3">
-          Designed & Built with <iconify-icon icon="lucide:heart" />
+          Designed & Built with <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="21"
+            height="21"
+            viewBox="0 0 24 24">
+            <path
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2c-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" />
+          </svg>
           by
         </small>
         <address>Santiago</address>
@@ -154,12 +183,30 @@
         href={site.site.repo}
         aria-label="Link to git repository for this site at Github"
         title="Link to git repository for this site at Github">
-        Repository <iconify-icon
-          aria-label="GitHub icon"
-          role="img"
-          icon="simple-icons:github" />
-        <div aria-orientation="vertical" role="separator">--</div>
-        <iconify-icon icon="lucide:star" />
+        Repository
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24">
+          <path
+            fill="currentColor"
+            d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2Z" />
+        </svg>
+        <div aria-orientation="vertical" role="separator">-</div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="21"
+          height="21"
+          viewBox="0 0 24 24">
+          <path
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m12 2l3.09 6.26L22 9.27l-5 4.87l1.18 6.88L12 17.77l-6.18 3.25L7 14.14L2 9.27l6.91-1.01L12 2z" />
+        </svg>
         <var
           style="margin-top: 1px"
           aria-label="Amount of stars in the GitHub repository"
@@ -169,7 +216,22 @@
       </a>
 
       <small class="font-3 copyright">
-        (c) {new Date().getFullYear()} -- present
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="21"
+          height="21"
+          viewBox="0 0 24 24">
+          <g
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2">
+            <circle cx="12" cy="12" r="10" />
+            <path d="M15 9.354a4 4 0 1 0 0 5.292" />
+          </g>
+        </svg>
+        2023 — present
         <div aria-orientation="vertical" role="separator">•</div>
         All rights reserved
         <div aria-orientation="vertical" role="separator">•</div>
