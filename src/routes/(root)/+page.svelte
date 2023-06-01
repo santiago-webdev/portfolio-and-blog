240904ad6dddd967c31030486fd2174841f71c4b
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-06-01 12:19:46 -0300

    Redo projects section in landing page

--- src/routes/(root)/+page.svelte
+++ src/routes/(root)/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import site from '$lib/site.json';
 	import AboveTheFold from './-AboveTheFold.svelte';
-	// import Portfolio from './-Portfolio.svelte';
+	import Portfolio from './-Portfolio.svelte';
 	import Blog from './-Blog.svelte';
 	import About from './-About.svelte';
 </script>
 
 <AboveTheFold />
-<!-- <Portfolio /> -->
+<Portfolio />
 <Blog />
 <About />
 
--- src/routes/(root)/-Portfolio.svelte
+++ src/routes/(root)/-Portfolio.svelte
@@ -1,70 +1,159 @@
+<script>
+	import LinkUI from '$lib/components/navigation/LinkUI.svelte';
+	import { base } from '$app/paths';
+</script>
+
 <div class="wrapper-portfolio">
 	<div id="portfolio" class="portfolio">
+		<ol>
+			<li>
+				<a href="/">
+					<h3 class="font-40">
+						Project
+						<iconify-icon icon="lucide:external-link" />
+					</h3>
+					<p class="font-20">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+				</a>
+			</li>
+			<li>
+				<a href="/">
+					<h3 class="font-40">
+						Project
+						<iconify-icon icon="lucide:external-link" />
+					</h3>
+					<p class="font-20">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+				</a>
+			</li>
+			<li>
+				<a href="/">
+					<h3 class="font-40">
+						Project
+						<iconify-icon icon="lucide:external-link" />
+					</h3>
+					<p class="font-20">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+				</a>
+			</li>
+		</ol>
 		<section>
-			<span class="font-10"> Featured Project &mdash; </span>
-			<h2>Featured Project</h2>
+			<h2>Latest projects</h2>
+			<p>I've contributed to FOSS as well as created some projects of my own</p>
 			<p>
-				Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur
-				cupidatat.
+				More projects with details can be tracked <LinkUI
+					style="display: inline-flex; font-size: inherit; line-height: inherit;"
+					href="{base}/portfolio">in the dedicated page</LinkUI
+				>
 			</p>
 		</section>
-		<div class="img-wrapper">
-			<img loading="lazy" src="/portfolio/zap-website.webp" alt="" />
-		</div>
 	</div>
 </div>
 
 <style>
 	.wrapper-portfolio {
 		background-color: var(--clr-05);
-		color: var(--clr-200);
 	}
 
 	.portfolio {
 		display: flex;
+		flex-wrap: wrap-reverse;
 		width: min(92%, var(--md));
 		min-height: 100vh;
+		min-height: 100svh;
 		margin-inline: auto;
 		padding: 3rem 0;
 		gap: 3rem;
 		place-items: center;
-	}
-
-	span {
-		display: flex;
-		gap: 0.4rem;
-		color: var(--clr-95);
-		font-variation-settings: 'wght' 800;
-	}
-
-	.img-wrapper {
-		flex: 1;
-		max-height: 40rem;
-		overflow: hidden;
-		border-radius: 1rem;
-		border: 1px solid var(--clr-25);
-	}
-
-	img {
-		max-inline-size: 100%;
-		overflow: hidden;
+		place-content: center;
 	}
 
 	section {
 		display: flex;
 		flex-direction: column;
 		gap: 0.6rem;
-		margin: 2rem;
+		flex: 1;
 	}
 
-	p {
+	section > * + * {
+		margin-block-start: 0.8em;
+	}
+
+	section p {
 		color: var(--clr-80);
 		max-width: 38ch;
+	}
+
+	ol {
+		flex: 1;
+		display: grid;
+		gap: 1rem;
+		font-family: var(--ff-dflt);
+	}
+
+	li {
+		min-width: var(--xs);
+		border-radius: 0.6rem;
+	}
+
+	li a {
+		display: grid;
+		gap: 0.4rem;
+		padding: 0.8rem;
+	}
+
+	h3 {
+		display: flex;
+		place-items: center;
+		gap: 1ch;
+		/* font-weight: 500; */
+		font-variation-settings: 'wght' 700;
 	}
 
 	@media screen and (min-height: 80rem) {
 		.portfolio {
 			min-height: 0;
 		}
+	}
+
+	@media screen and (min-width: 1024px) {
+		li a {
+			gap: 0.8rem;
+			padding: 1.6rem;
+		}
+	}
+
+	/* Gradient styles for each list items. */
+	li:first-child {
+		background-color: var(--clr-35);
+	}
+
+	li:first-child h3 {
+		color: var(--clr-300);
+	}
+
+	li:first-child p {
+		color: var(--clr-200);
+	}
+
+	li:nth-child(2) {
+		background-color: var(--clr-25);
+	}
+
+	li:nth-child(2) h3 {
+		color: var(--clr-250);
+	}
+
+	li:nth-child(2) p {
+		color: var(--clr-200);
+	}
+
+	li:last-child {
+		background-color: var(--clr-15);
+	}
+
+	li:last-child h3 {
+		color: var(--clr-250);
+	}
+
+	li:last-child p {
+		color: var(--clr-150);
 	}
 </style>
