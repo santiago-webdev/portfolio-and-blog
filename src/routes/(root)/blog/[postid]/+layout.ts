f16497d6447c6af734ae973737322f31cf5c38c6
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-06-03 08:38:09 -0300

    Format

--- src/routes/(root)/blog/[postid]/+layout.ts
+++ src/routes/(root)/blog/[postid]/+layout.ts
@@ -1,24 +1,22 @@
-import { Posts } from '$lib/components/search/store'
-import { get } from 'svelte/store'
-import type { LayoutLoad } from './$types'
+import { Posts } from '$lib/components/search/store';
+import { get } from 'svelte/store';
+import type { LayoutLoad } from './$types';
 
 export const load = (async ({ params }) => {
-  const [localPost] = get(Posts).filter((obj) => {
-    if (obj.href.replace('/', '') === (params.postid as string)) {
-      return obj
-    }
-  })
+	const [localPost] = get(Posts).filter((obj) => {
+		if (obj.href.replace('/', '') === (params.postid as string)) {
+			return obj;
+		}
+	});
 
-  const module = await import(
-    `../../../../lib/posts/${params.postid}.${localPost.extension}`
-  )
-  const content = module.default
-  const title = module.metadata.title
-  const description = module.metadata.description
+	const module = await import(`../../../../lib/posts/${params.postid}.${localPost.extension}`);
+	const content = module.default;
+	const title = module.metadata.title;
+	const description = module.metadata.description;
 
-  return {
-    content,
-    title,
-    description
-  }
-}) satisfies LayoutLoad
+	return {
+		content,
+		title,
+		description
+	};
+}) satisfies LayoutLoad;
