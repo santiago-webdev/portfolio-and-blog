c2d8990ce3e62d2a245eea29115441fb6ba226ff
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-06-05 19:28:17 -0300

    Use svgs in `WidgetModal`

--- src/lib/components/search/WidgetModal.svelte
+++ src/lib/components/search/WidgetModal.svelte
@@ -41,7 +41,18 @@
 
 <button aria-keyshortcuts="Control+K" aria-label="Click to open and search" on:click={toggleModal}>
 	<span class="font-20">&nbsp;&nbsp</span>
-	<iconify-icon icon="lucide:search" />
+	<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+		<g
+			fill="none"
+			stroke="currentColor"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="2"
+		>
+			<circle cx="11" cy="11" r="8" />
+			<path d="m21 21l-4.35-4.35" />
+		</g>
+	</svg>
 	<kbd class="font-20">Ctrl K</kbd>
 </button>
 <dialog
