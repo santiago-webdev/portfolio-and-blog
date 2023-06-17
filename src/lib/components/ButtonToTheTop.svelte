32da7634e36f993a31df92f8a726d29775ff72d5
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-06-16 22:19:10 -0300

    Don't modify the url in

--- src/lib/components/ButtonToTheTop.svelte
+++ src/lib/components/ButtonToTheTop.svelte
@@ -19,7 +19,13 @@
 <svelte:window bind:scrollY bind:innerHeight />
 
 <section>
-	<a style:visibility aria-label="Go to the top of the page" class="tpl reverse" href="#body">
+	<a
+		on:click|preventDefault={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+		style:visibility
+		aria-label="Go to the top of the page"
+		class="tpl reverse"
+		href="#body"
+	>
 		<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
 			<path
 				fill="none"
