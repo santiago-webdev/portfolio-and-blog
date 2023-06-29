2ed4a97d19404ee2ece113eafa40fad60ffa63d4
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-06-28 23:59:34 -0300

    enable transitions again

--- src/routes/(root)/+layout.svelte
+++ src/routes/(root)/+layout.svelte
@@ -2,33 +2,29 @@
 import NavigationBar from '$lib/components/navigation/NavigationBar.svelte';
 import ButtonToTheTop from '$lib/components/ButtonToTheTop.svelte';
 import Footer from '$lib/components/Footer.svelte';
-// import { fly } from 'svelte/transition';
+import { fly } from 'svelte/transition';
 import Seo from '$lib/seo.svelte';
 import { page } from '$app/stores';
 
-// export let data;
+export let data;
 </script>
 
 <div id="layout">
   <NavigationBar />
-  <slot />
-  <!-- {#key data.pathname} -->
-  <!-- 	<div -->
-  <!-- 		in:fly={{ y: -60, duration: 300, delay: 300 }} -->
-  <!-- 		out:fly={{ y: -60, duration: 300 }} -->
-  <!-- 	> -->
-  <!-- 		<slot /> -->
-  <!-- 	</div> -->
-  <!-- {/key} -->
+  {#key data.pathname}
+    <div
+      in:fly={{ y: -60, duration: 300, delay: 300 }}
+      out:fly={{ y: -60, duration: 300 }}
+    >
+      <slot />
+    </div>
+  {/key}
   <ButtonToTheTop />
   <Footer />
 </div>
 <Seo
   url={$page.data.pathname}
-  data={{
-    title: $page.data.title,
-    description: $page.data.description
-  }}
+  data={{ title: $page.data.title, description: $page.data.description }}
 />
 
 <style>
