e259380990b51d65e9eead2b242ce3905001e586
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-07-22 16:03:05 -0300

    format

--- uno.config.ts
+++ uno.config.ts
@@ -99,6 +99,5 @@ export default defineConfig({
  dark:bg-dark-600 dark:border-dark-700 dark:border-t-solid dark:border-l-solid border-1
 `,
     'inline-items': `inline-flex place-items-center flex-wrap gap-1`
-
   }
 });
