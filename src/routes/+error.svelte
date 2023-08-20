44a460818431574d54163691cf459dcff8ee6e37
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-08-19 23:54:15 -0300

    update error page, remove unocss classes

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -6,13 +6,30 @@ import ShapeButton from '$lib/components/navigation/ShapeButton.svelte';
 import config from '$lib/config.json';
 </script>
 
-<main
-  class="min-h-screen flex flex-col max-w-screen-lg px-8 mx-auto justify-center items-center text-center"
->
+<main>
   <h1 class="font-fluid-4">{$page.status}: {$page.error?.message}</h1>
-  <p class="mt-4 mb-8">
+  <p>
     Something has gone wrong, if you'd like to report it, please
     <ShapeAnchor href="{config.landingpage.repo}/issues">open an issue</ShapeAnchor>.
   </p>
   <ShapeButton class="default" href="{base}/">Back to Home</ShapeButton>
 </main>
+
+<style>
+main {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  padding: 0 1.5rem;
+  margin-inline: auto;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  width: min(100% 2rem, var(--md));
+}
+
+p {
+  margin-top: 1rem;
+  margin-bottom: 1.5rem;
+}
+</style>
